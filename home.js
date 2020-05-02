426013eca2a026b893bca3fbc28f11153aed177c
arash <arash.mokhtari.k@gmail.com>
2020-05-02 17:14:09 +0200

    How to use object!

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
-showMessage("Test message from the function!");
+//showMessage("Test message from the function!");
+let person = {
+    name: "Arash",
+    age: 33,
+    sex: "male",
+    showInfo: function() {
+        return (this.name + " is " + this.age);
+    }
+};
+showMessage(person.showInfo());
